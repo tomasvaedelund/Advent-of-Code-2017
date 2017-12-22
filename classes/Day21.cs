e051c55df2447bf98f74dea8cc98a29fde829bb4
tomas.vaedelund <tvaedelund@gmail.com>
2017-12-22 17:27:04 +0100

    Day 21 - First star: Rules in place, juts need to check them now...

--- classes/Day21.cs
+++ classes/Day21.cs
@@ -1,27 +1,60 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Advent_of_Code_2017.classes
 {
     public static class Day21
     {
+
+        private static string[] Matrix = {
+            ".#.",
+            "..#",
+            "###"
+        };
+
         public static void GetResult()
         {
-            var test = ".#./.##/##.";
+            Debug.Assert(GetLitPixelsAfterNIterations(2) == 12);
 
-            var array = test.Split('/');
+            var result = "";
 
-            Print(array);
-            Print(array.FlipHorizontally());
-            Print(array.FlipVertically());
-            Print(array.Transpose());
-            Print(array.Rotate90());
+            var stopWatch = Stopwatch.StartNew();
+            //result = GetNumberOfBurstsThatCauseInfection(10000, data).ToString();
+            stopWatch.Stop();
+            Helpers.DisplayDailyResult("22 - 1", result, stopWatch.ElapsedMilliseconds);
+
         }
 
-        static void Print(IEnumerable<string> value)
+        private static IEnumerable<(IEnumerable<string> pattern, IEnumerable<string> rule)> Rules = GetRules();
+
+        private static int GetLitPixelsAfterNIterations(int iterations)
         {
-            Console.WriteLine(string.Join("/", value));
+            while (iterations-- > 0)
+            {
+                
+            }
+
+            return string.Join("", Matrix).Count(c => c == '#');
+        }
+
+        private static IEnumerable<(IEnumerable<string> pattern, IEnumerable<string> rule)> GetRules()
+        {
+            var rules = Helpers.GetDataFromFile("day21.txt");
+
+            foreach (var rule in rules.Split("\r\n"))
+            {
+                var p = rule.Split(" => ")[0].Split('/');
+                var r = rule.Split(" => ")[1].Split('/');
+
+                yield return (p, r);
+                yield return (p.FlipVertically(), r);
+                yield return (p.FlipHorizontally(), r);
+                yield return (p.Rotate90(), r);
+                yield return (p.Rotate180(), r);
+                yield return (p.Rotate270(), r);
+            }
         }
 
         private static IEnumerable<string> FlipHorizontally(this IEnumerable<string> rows)
@@ -38,6 +71,22 @@ namespace Advent_of_Code_2017.classes
         {
             rows = rows.Transpose();
             rows = rows.FlipVertically();
+
+            return rows;
+        }
+
+        private static IEnumerable<string> Rotate180(this IEnumerable<string> rows)
+        {
+            rows = rows.FlipVertically();
+            rows = rows.FlipHorizontally();
+
+            return rows;
+        }
+
+        private static IEnumerable<string> Rotate270(this IEnumerable<string> rows)
+        {
+            rows = rows.Rotate180();
+            rows = rows.Rotate90();
 
             return rows;
         }
